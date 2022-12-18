7f113974f1a82cf7c8b928215038eaf0cc803904
estebbins <stebbins.e@gmail.com>
2022-12-18 13:39:15 -0500

    Finished Deliverable

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-// Need to respond to a click on a box and add a symbol to indicate a move
-// once this has occurred, it should automatically switch to the next player
-// Need to start with player1 and after they click on a box, it goes to player 2
-// Need to establish which symbol is player 1 and which is player 2
-// Need to switch symbols from player 1 to player 2
-// Evaluate if a player wins loses or ties
-
 // Researched how to compare arrays: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 
 // Create DOM constants
